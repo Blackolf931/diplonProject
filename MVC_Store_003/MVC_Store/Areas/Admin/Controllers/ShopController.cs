0f19e80657b0d7eb41f0e025ff86aa4bd3beb0cd
forstudyonly <62891034+forstudyonly@users.noreply.github.com>
2021-02-03 13:19:43 +0300

    V008

---
+++ MVC_Store_003/MVC_Store/Areas/Admin/Controllers/ShopController.cs
@@ -0,0 +1,33 @@
+﻿using MVC_Store.Models.Data;
+using MVC_Store.Models.ViewModels.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Store.Areas.Admin.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Admin/Shop
+        public ActionResult Categories()
+        {
+            //Declare the model type of List
+            List<CategoryVM> categoryVMList;
+
+            using (Db db = new Db())
+            {
+
+                //intialize the model with data 
+                categoryVMList = db.Categories.ToArray()
+                    .OrderBy(x => x.Sorting)
+                    .Select(x => new CategoryVM(x))
+                    .ToList();
+
+            }
+            //return List in View
+            return View(categoryVMList);
+        }
+    }
+}
