a8495772adaa582cbf297802441c1e238196d49b
forstudyonly <62891034+forstudyonly@users.noreply.github.com>
2021-02-04 17:02:32 +0300

    V009

--- MVC_Store_003/MVC_Store/Areas/Admin/Controllers/ShopController.cs
+++ MVC_Store_003/MVC_Store/Areas/Admin/Controllers/ShopController.cs
@@ -29,5 +29,74 @@ namespace MVC_Store.Areas.Admin.Controllers
             //return List in View
             return View(categoryVMList);
         }
+
+         //Post: Admin/Sho/AddNewCategory 
+         [HttpPost]
+        public string AddNewCategory(string catName)
+        {
+            // Declare a new param ID
+            string id;
+            
+            using(Db db = new Db())
+            {
+                //Check NameCategory on Unique
+                if (db.Categories.Any(x => x.Name == catName))
+                    return "titletaken";
+                //initializa a model DTO
+                CategoryDTO dto = new CategoryDTO();
+
+                //Fill data in a model
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 100;
+                //Save model
+                db.Categories.Add(dto);
+                db.SaveChanges();
+                //Get Id
+                id = dto.Id.ToString();
+
+            }
+
+            //return ID in VM
+            return id;
+        }
+
+        //Ger: Admin/Shop/ReorderCategories
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                int count = 1;
+                CategoryDTO dto;
+                foreach (var catId in id)
+                {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+                    db.SaveChanges();
+                    count++;
+                }
+            }
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            using (Db db = new Db())
+            {
+                //Get Category
+                CategoryDTO dto = db.Categories.Find(id);
+                //Delete Category
+                db.Categories.Remove(dto);
+
+                //Save changes in database
+                db.SaveChanges();
+            }
+            //Add message about succesfull delete
+            TempData["SM"] = "You have deleted a category!";
+
+            //Redirect user to index
+
+            return RedirectToAction("Categories");
+        }
     }
 }
