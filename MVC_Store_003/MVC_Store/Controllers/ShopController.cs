ab7949c87bd6569abe03b42e76bab0b531c14e30
forstudyonly <62891034+forstudyonly@users.noreply.github.com>
2021-04-02 20:48:49 +0300

    V019

--- MVC_Store_003/MVC_Store/Controllers/ShopController.cs
+++ MVC_Store_003/MVC_Store/Controllers/ShopController.cs
@@ -2,6 +2,7 @@
 using MVC_Store.Models.ViewModels.Shop;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -27,6 +28,73 @@ namespace MVC_Store.Controllers
             }
             //return View with model
             return PartialView("_CategoryMenuPartial", categoryVMList);
+        }
+        //GET: Shop/Category/name
+
+            //Get Category
+        public ActionResult Category(string name)
+        {
+            //Declare List of tupe List<>
+            List<ProductVM> productVMList;
+            //Get idCategory
+            using (Db db = new Db())
+            {
+                CategoryDTO dto = db.Categories.Where(x => x.Slug == name).FirstOrDefault();
+                int catId = dto.Id;
+                //Initialize a list of data
+                productVMList = db.Products.ToArray().Where(x => x.CategoryId == catId).Select(x => new ProductVM(x)).ToList();
+                //Get a name category
+                var productCat = db.Products.Where(x => x.CategoryId == catId).FirstOrDefault();
+                //Check on null
+                if(productCat == null)
+                {
+                    var catName = db.Categories.Where(x => x.Slug == name).Select(x => x.Name).FirstOrDefault();
+                    ViewBag.CategoryName = catName;
+                }
+                else
+                {
+                    ViewBag.CategoryName = productCat.CategoryName;
+                }
+            }
+            //return Vm with model
+            return View(productVMList);
+        }
+
+        //GET: Shop/product-details/name
+        [ActionName("product-details")]
+        public ActionResult ProductDetails(string name)
+        {
+
+            //Declare DTO and VM
+            ProductDTO dto;
+            ProductVM model;
+
+            //Initialize idProduct
+            int id = 0;
+
+            //Check on usability
+            using (Db db = new Db())
+            {
+
+                if (!db.Products.Any(x => x.Slug.Equals(name)))
+                {
+                    return RedirectToAction("Index", "shop");
+                }
+
+                //Initialize model ProductDTO data
+                dto = db.Products.Where(x => x.Slug == name).FirstOrDefault();
+
+                //Get idProduct
+                id = dto.Id;
+
+                //Initialize the model VM with data
+                model = new ProductVM(dto);
+            }
+            //Get all images from Gallery
+            model.GalleryImages = Directory.EnumerateFiles(Server.MapPath("~/Images/Uploads/Products/" + id + "/Gallery/Thumbs"))
+                .Select(fn => Path.GetFileName(fn));
+            //Return the model in VM
+            return View("ProductDetails", model);
         }
     }
 }
